46bdcf1e3400e6654d76b2e9b22fb3dd89c1c9a8
Pavel Rudko <icpavelrudko@gmail.com>
2015-03-30 01:09:00 +0300

    Entity Framework Support

--- Free-Pay-Service/IPaymentService.cs
+++ Free-Pay-Service/IPaymentService.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
-namespace Free_Pay_Service
+namespace FreePayService
 {
     [ServiceContract]
     public interface IPaymentService
--- Free-Pay-Service/PaymentService.svc.cs
+++ Free-Pay-Service/PaymentService.svc.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using FreePayService.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
 
-namespace Free_Pay_Service
+namespace FreePayService
 {
     public class PaymentService : IPaymentService
     {
         public String GetMessage()
         {
-            return "Hello world!";
+            String name = "unknown";
+            using (var context = new PaymentsContext())
+            {
+                name = context.Persons.ToList().First().Name;
+            }
+            return "Hello from " + name;
         }
+    }
+
+    public class PaymentsContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
     }
 }
