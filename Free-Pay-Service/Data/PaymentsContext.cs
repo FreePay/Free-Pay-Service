a7dc88d2efba5d861e0e3ac46c5dcaca071c5f74
Pavel Rudko <icpavelrudko@gmail.com>
2015-04-11 09:40:38 +0200

    Service Registration

---
+++ Free-Pay-Service/Data/PaymentsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FreePayService.Models;
+
+namespace FreePayService.Data
+{
+    public class PaymentsContext : DbContext
+    {
+        public DbSet<WebServiceInfo> WebServices { get; set; }
+    }
+}
---
+++ Free-Pay-Service/Services/IRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿using FreePayService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FreePayService.Services
+{
+    [ServiceContract]
+    public interface IRegistrationService
+    {
+        [OperationContract]
+        ServiceRegistrationResult RegisterService(string name, double price);
+    }
+}
