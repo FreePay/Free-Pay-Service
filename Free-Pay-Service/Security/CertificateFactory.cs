e49f448a34b873b5e059679e72c2b5c63eba7ef3
Pavel Rudko <icpavelrudko@gmail.com>
2015-05-24 18:54:14 +0300

    Change store

--- Free-Pay-Service/Security/CertificateFactory.cs
+++ Free-Pay-Service/Security/CertificateFactory.cs
@@ -11,7 +11,7 @@ namespace FreePayService.Security
     {
         public static X509Certificate2 GetCertificate(string name)
         {
-            var store = new X509Store(StoreLocation.CurrentUser);
+            var store = new X509Store(StoreLocation.LocalMachine);
             store.Open(OpenFlags.ReadOnly);
             try
             {
